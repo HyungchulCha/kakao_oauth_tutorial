d41606f1ffa98cb87bd341e3b9fcd3399a236e5b
HyungchulCha <ckgudcjf@gmail.com>
2021-06-16 23:22:09 +0900

    init

---
+++ server/src/config.js
@@ -0,0 +1,8 @@
+export const GOOGLE_CLIENT_ID =
+  process.env.GOOGLE_CLIENT_ID || '766398922199-jdd7cpd2ia1lhb5c00r53b52fqme3fpe.apps.googleusercontent.com';
+export const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_SECRET_ID || 'Jajht6qy09FcEsChA_RiBUbp';
+export const SERVER_ROOT_URI = 'http://localhost:4000';
+export const SERVER_REDIRECT_URI = '/auto/google';
+export const UI_ROOT_URI = 'http://localhost:3000';
+export const JWT_SECRET = 'shhhhh';
+export const COOKIE_NAME = 'auth_token';
---
+++ server/src/index.js
@@ -0,0 +1,114 @@
+import express from 'express';
+import jwt from 'jsonwebtoken';
+import axios from 'axios';
+import cors from 'cors';
+import querystring from 'querystring';
+import cookieParser from 'cookie-parser';
+import {
+  COOKIE_NAME,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
+  JWT_SECRET,
+  SERVER_REDIRECT_URI,
+  SERVER_ROOT_URI,
+  UI_ROOT_URI,
+} from './config';
+
+const port = 4000;
+const app = express();
+
+app.use(
+  cors({
+    origin: UI_ROOT_URI,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+
+function getGoogleAuthURL() {
+  const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
+  const options = {
+    redirect_uri: `${SERVER_ROOT_URI}${SERVER_REDIRECT_URI}`,
+    client_id: GOOGLE_CLIENT_ID,
+    access_type: 'offline',
+    response_type: 'code',
+    prompt: 'consent',
+    scope: ['https://www.googleapis.com/auth/userinfo.profile', 'https://www.googleapis.com/auth/userinfo.email'].join(
+      ' '
+    ),
+  };
+  return `${rootUrl}?${querystring.stringify(options)}`;
+}
+
+// Getting login URL
+app.get('/auth/google/url', (req, res) => {
+  return res.send(getGoogleAuthURL());
+});
+
+function getTokens({ code, clientId, clientSecret, redirectUri }) {
+  const url = 'https://oauth2.googleapis.com/token';
+  const values = {
+    code,
+    client_id: clientId,
+    client_secret: clientSecret,
+    redirect_uri: redirectUri,
+    grant_type: 'authorization_code',
+  };
+  return axios
+    .post(url, querystring.stringify(values), {
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+    })
+    .then((res) => res.data)
+    .catch((err) => {
+      console.error('Failed to fetch auth tokens');
+      throw new Error(err.message);
+    });
+}
+
+// Getting the user from Google with the code
+app.get(`${SERVER_REDIRECT_URI}`, async (req, res) => {
+  const code = req.query.code;
+  const { id_token, access_token } = await getTokens({
+    code,
+    clientId: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    redirectUri: `${SERVER_ROOT_URI}${SERVER_REDIRECT_URI}`,
+  });
+  const googleUser = await axios
+    .get(`https://www.googleapis.com/oauth2/v1/user/userinfo?alt=json&access_token=${access_token}`, {
+      headers: {
+        Authorization: `Bearer ${id_token}`,
+      },
+    })
+    .then((res) => res.data)
+    .catch((err) => {
+      console.error('Failed to fetch user');
+      throw new Error(err.message);
+    });
+  const token = jwt.sign(googleUser, JWT_SECRET);
+  res.cookie(COOKIE_NAME, token, {
+    maxAge: 900000,
+    httpOnly: true,
+    secure: false,
+  });
+  res.redirect(UI_ROOT_URI);
+});
+
+// Getting the current user
+app.get('/auth/me', (req, res) => {
+  console.log('get me');
+  try {
+    const decoded = jwt.verify(req.cookie[COOKIE_NAME], JWT_SECRET);
+    console.log('decoded: ', decoded);
+    return res.send(decoded);
+  } catch (err) {
+    console.log(err);
+    res.send(null);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`App listening http://localhost:${port}`);
+});
